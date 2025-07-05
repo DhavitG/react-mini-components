5bfd78c3f642fea15e128fbf184f588d2e3bbb70
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-05 20:29:38 +0530

    Loading more products from an API done

--- react-mini-components/src/Components/LoadMoreData.jsx
+++ react-mini-components/src/Components/LoadMoreData.jsx
@@ -1,31 +1,67 @@
 import { useEffect, useState } from "react";
+import "../Styles/LoadMoreData.css";
 
 export default function LoadMoreData() {
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
   async function fetchProducts() {
     try {
-      setIsLoading(true)
-      
+      setIsLoading(true);
+      console.log("inside fetch products");
+
       const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
+        `https://dummyjson.com/products?limit=20&skip=${count * 20}`
       );
-      const result = await response.json()
-      
-      if (result && result.products && result.length) {
-        setProducts(result.products)
-        setIsLoading(false)
+
+      const result = await response.json();
+
+      if (result && result.products) {
+        setProducts((prev) => [...prev, ...result.products]);
       }
+      setIsLoading(false);
+
+      console.log("Rendering component with", products.length, "products");
     } catch (e) {
       console.log(e);
+      setIsLoading(false);
     }
   }
 
-  useEffect(() => fetchProducts(), []);
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
 
-  return <div className="container"></div>;
+  useEffect(() => {
+    if (products && products.length >= 100) setDisableButton(true);
+  }, [products]);
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item, index) => {
+              return (
+                <div key={item.id} className="product">
+                  <img src={item.thumbnail} alt={item.title} />
+                  <p>{item.title}</p>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <div>
+        <button
+          disabled={disableButton}
+          onClick={() => setCount((count) => count + 1)}
+          className="button-container"
+        >
+          Load more products
+        </button>
+        {disableButton ? "You have reached maximum products" : null}
+      </div>
+    </div>
+  );
 }
